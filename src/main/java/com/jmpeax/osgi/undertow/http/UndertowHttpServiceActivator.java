2a9e6b0340b1d05e57efbef6c93f168cb2205001
Carlos Ortiz <carlos.ortiz@craftercms.org>
2014-09-30 20:40:11 -0600

    Initial Commit

---
+++ src/main/java/com/jmpeax/osgi/undertow/http/UndertowHttpServiceActivator.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2007-2014 JMPEAX.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.jmpeax.osgi.undertow.http;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ *
+ */
+public class UndertowHttpServiceActivator implements BundleActivator {
+    @Override
+    public void start(final BundleContext context) throws Exception {
+
+    }
+
+    @Override
+    public void stop(final BundleContext context) throws Exception {
+
+    }
+}
